f1a959073f9a5b73bf93f2d4427650a531e5eb57
Anthony Grayling <anthony.grayling.milliondoll1216@hotmail.fr>
2024-06-03 21:10:25 +0200

    CSS modified for likes icons

--- src/models/ImageMedia.js
+++ src/models/ImageMedia.js
@@ -11,8 +11,10 @@ class ImageMedia extends Media {
       <img class="img_display" src="${this.image}" alt="${this.title}" title="${this.title}" tabindex="0">
       <div class="personal-photos">
         <p class="photo-details">${this.title}</p>
+      <div class="likes">
         <p class="photo-details like-count" data-likes="${this.likes}">${this.likes}</p>
         <i class="fa-solid fa-heart like-button" data-id="${this.id}" data-liked="false"></i>
+      </div>
       </div>`;
   }
 }
--- src/models/VideoMedia.js
+++ src/models/VideoMedia.js
@@ -11,8 +11,10 @@ class VideoMedia extends Media {
       <video class="vids" controls src="${this.video}" alt="${this.title}" title="${this.title}" tabindex="0"></video>
       <div class="personal-photos">
         <p class="photo-details">${this.title}</p>
+        <div class="likes">
         <p class="photo-details like-count" data-likes="${this.likes}">${this.likes}</p>
         <i class="fas fa-heart heart-icon like-button" data-id="${this.id}" data-liked="false"></i>
+        </div>
       </div>`;
   }
 }
