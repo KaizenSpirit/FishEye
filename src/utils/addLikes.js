b5919180737de9aecdd4807fcbc75f30be84076d
Anthony Grayling <anthony.grayling.milliondoll1216@hotmail.fr>
2024-06-05 15:43:44 +0200

    modified addLikesListeners function in addLikes file in order toallow user to like and unlike medias. Heart turns to red color when like and in white as a dislike

--- src/utils/addLikes.js
+++ src/utils/addLikes.js
@@ -3,16 +3,27 @@ export function addLikeListeners(price, updateTotalLikes) {
   likeButtons.forEach(button => {
     button.addEventListener('click', (event) => {
       event.stopPropagation(); // Arrête la propagation de l'événement de clic
-      const liked = event.target.getAttribute('data-liked') === 'true';
-      if (!liked) {
-        const likesSpan = event.target.previousElementSibling;
-        let likes = parseInt(likesSpan.innerText);
+      const icon = event.target; // Icone coeur cliqué
+      const liked = icon.getAttribute('data-liked') === 'true';
+      const likesSpan = icon.previousElementSibling;
+      let likes = parseInt(likesSpan.innerText);
+
+      if (liked) {
+        // Décrémente le nombre de likes et change la couleur de l'icône à l'original
+        likes -= 1;
+        icon.setAttribute('data-liked', 'false');
+        icon.classList.remove('liked');
+      } else {
+        // Incrémente le nombre de likes et change la couleur de l'icône en rouge
         likes += 1;
-        likesSpan.innerText = likes;
-        event.target.setAttribute('data-liked', 'true');
-        updateTotalLikes(price); 
+        icon.setAttribute('data-liked', 'true');
+        icon.classList.add('liked');
       }
+
+      likesSpan.innerText = likes;
+      updateTotalLikes(price); 
     });
   });
 }
+
 
