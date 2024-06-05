a032047b19afec8974f5ec14be66c72ca78a9ab4
Anthony Grayling <anthony.grayling.milliondoll1216@hotmail.fr>
2024-06-05 18:28:45 +0200

    addLikeListeners function modified in addLikes file. Focus now possible on heart icons for increasing or decreasing likes by one.

--- src/utils/addLikes.js
+++ src/utils/addLikes.js
@@ -1,29 +1,38 @@
 export function addLikeListeners(price, updateTotalLikes) {
   const likeButtons = document.querySelectorAll('.like-button');
+
   likeButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-      event.stopPropagation(); // Arrête la propagation de l'événement de clic
-      const icon = event.target; // Icone coeur cliqué
+    button.setAttribute('tabindex', '0');
+
+    const toggleLike = (event) => {
+      event.stopPropagation(); 
+      const icon = event.target;
       const liked = icon.getAttribute('data-liked') === 'true';
       const likesSpan = icon.previousElementSibling;
       let likes = parseInt(likesSpan.innerText);
 
       if (liked) {
-        // Décrémente le nombre de likes et change la couleur de l'icône à l'original
         likes -= 1;
         icon.setAttribute('data-liked', 'false');
         icon.classList.remove('liked');
       } else {
-        // Incrémente le nombre de likes et change la couleur de l'icône en rouge
         likes += 1;
         icon.setAttribute('data-liked', 'true');
         icon.classList.add('liked');
       }
 
       likesSpan.innerText = likes;
-      updateTotalLikes(price); 
+      updateTotalLikes(price);
+    };
+    button.addEventListener('click', toggleLike);
+    
+    button.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        toggleLike(event);
+      }
     });
   });
 }
+
 
 
